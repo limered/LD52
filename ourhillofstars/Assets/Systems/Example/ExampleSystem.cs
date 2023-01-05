0616810f7b74ab4e3ce2c9df3c376efa739799b3
Emil Bohleber <me@limered.de>
2023-01-05 19:57:25 +0100

    add a simple example

---
+++ ourhillofstars/Assets/Systems/Example/ExampleSystem.cs
@@ -0,0 +1,29 @@
+﻿using SystemBase.Core;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
+namespace Systems.Example
+{
+    [GameSystem]
+    public class ExampleSystem : GameSystem<ExampleComponent>
+    {
+        private Random _rnd = new(123);
+        
+        public override void Register(ExampleComponent component)
+        {
+            component.UpdateAsObservable()
+                .Where(_ => Time.frameCount % 20 == 0)
+                .Subscribe(_ => Animate(component))
+                .AddTo(component);
+        }
+
+        private void Animate(ExampleComponent component)
+        {
+            var dir = _rnd.NextFloat3() * component.speed;
+            
+            component.GetComponent<Rigidbody>().AddForce(dir);
+        }
+    }
+}
