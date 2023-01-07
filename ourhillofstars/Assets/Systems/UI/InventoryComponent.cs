a3f4b9ef95b560081da2905f4301ceb46233e7ed
ThunfischEis <ian.risch@gmail.com>
2023-01-07 22:48:08 +0100

    Add Pause ButtonTextureSwitch

--- ourhillofstars/Assets/Systems/UI/InventoryComponent.cs
+++ ourhillofstars/Assets/Systems/UI/InventoryComponent.cs
@@ -1,7 +1,9 @@
+using System.Linq;
 using SystemBase.Core;
 using SystemBase.Utils;
 using Systems.Levels;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Systems.UI
 {
@@ -10,11 +12,17 @@ namespace Systems.UI
         public Sprite[] arrowSprites;
         public GameObject arrowElementPrefab;
         public GameObject arrows;
+        public Sprite primarySprite;
+        public Sprite secondarySprite;
+        private Image _image;
 
         public void StartDrescher()
         {
             var oldValue = IoC.Game.GetComponent<CurrentLevelComponent>().harvesterRunning.Value;
             IoC.Game.GetComponent<CurrentLevelComponent>().harvesterRunning.Value = !oldValue;
+            _image = GetComponentsInChildren<Image>().First(image => image.sprite.name == primarySprite.name);
+            _image.sprite = secondarySprite;
+            (secondarySprite, primarySprite) = (primarySprite, secondarySprite);
         }
     }
 }
