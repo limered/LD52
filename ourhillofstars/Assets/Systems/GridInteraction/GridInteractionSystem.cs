3956bf181a5d0018c84172fa35c9f43f487a636e
Emil Bohleber <me@limered.de>
2023-01-07 15:22:00 +0100

    add interaction plane

---
+++ ourhillofstars/Assets/Systems/GridInteraction/GridInteractionSystem.cs
@@ -0,0 +1,33 @@
+﻿using SystemBase.Core;
+using Systems.Grid;
+using Systems.GridRendering;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.GridInteraction
+{
+    [GameSystem]
+    public class GridInteractionSystem : GameSystem<MainGridComponent>
+    {
+        public override void Register(MainGridComponent component)
+        {
+            component.gridsInitialized
+                .Subscribe(InitializeInteractions)
+                .AddTo(component);
+        }
+
+        private void InitializeInteractions(MainGridComponent grid)
+        {
+            var fGrid = grid.foregroundGrid;
+            var fGridComponent = grid.GetComponentInChildren<ForegroundParentComponent>();
+
+            var ray = fGridComponent.mainCamera.ScreenPointToRay(Input.mousePosition);
+        
+            if (Physics.Raycast(ray, out var hit)) {
+                var objectHit = hit.transform.position;
+                
+                Debug.Log(objectHit.x);
+            }
+        }
+    }
+}
