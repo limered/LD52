386a4ce82d9c1a2a10d90026f29e88cd12ee3ef2
Emil Bohleber <me@limered.de>
2023-01-07 14:46:26 +0100

    split animation for foreground cells

--- ourhillofstars/Assets/Systems/GridRendering/GridRenderingSystem.cs
+++ ourhillofstars/Assets/Systems/GridRendering/GridRenderingSystem.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using SystemBase.Core;
 using SystemBase.Utils;
 using Systems.Grid;
 using UniRx;
 using UnityEngine;
-using Object = UnityEngine.Object;
-using Random = UnityEngine.Random;
 
 namespace Systems.GridRendering
 {
     [GameSystem]
-    public class GridRenderingSystem : GameSystem<MainGridComponent, BackgroundCellComponent>
+    public class GridRenderingSystem : GameSystem<MainGridComponent, BackgroundCellComponent, ForegroundCellComponent>
     {
         public override void Register(MainGridComponent component)
         {
@@ -41,15 +38,17 @@ namespace Systems.GridRendering
 
             var backgroundParent = grid.GetComponentInChildren<BackgroundParentComponent>();
             var foregroundParent = grid.GetComponentInChildren<ForegroundParentComponent>();
-            
+
             var max = grid.dimensions.x * grid.dimensions.y;
             for (var i = 0; i < max; i++)
             {
                 var x = i % grid.dimensions.x;
                 var y = i / grid.dimensions.x;
-                grid.backgroundCells[i] = Object.Instantiate(backgroundPrefab, new Vector3(x, 0, y), Quaternion.Euler(0, 180, 0),
+                grid.backgroundCells[i] = Object.Instantiate(backgroundPrefab, new Vector3(x, 0, y),
+                    Quaternion.Euler(0, 180, 0),
                     backgroundParent.transform);
-                grid.foregroundCells[i] = Object.Instantiate(foregroundPrefab, new Vector3(x, 0, y), Quaternion.Euler(0, 180, 0),
+                grid.foregroundCells[i] = Object.Instantiate(foregroundPrefab, new Vector3(x, 0, y),
+                    Quaternion.Euler(0, 180, 0),
                     foregroundParent.transform);
             }
         }
@@ -58,21 +57,23 @@ namespace Systems.GridRendering
         {
             component.rendererCache = component.GetComponent<Renderer>();
             component.type
-                .Subscribe(_ => AnimateCellChange(component))
+                .Subscribe(_ => AnimateBackgroundCellChange(component))
                 .AddTo(component);
         }
 
-        private void AnimateCellChange(BackgroundCellComponent backgroundCell)
+        private void AnimateBackgroundCellChange(BackgroundCellComponent cell)
         {
-            if (backgroundCell.type.Value != BackgroundCellType.Harvested)
-                Observable.FromMicroCoroutine(() => SwitchGridCell(backgroundCell))
+            if (cell.type.Value != BackgroundCellType.Harvested)
+                Observable.FromMicroCoroutine(() => SwitchGridCell(cell.gameObject,
+                        cell.rendererCache, cell.images[(int)cell.type.Value]))
                     .Subscribe()
-                    .AddTo(backgroundCell);
+                    .AddTo(cell);
             else
-                backgroundCell.rendererCache.material.mainTexture = backgroundCell.images[(int)backgroundCell.type.Value];
+                cell.rendererCache.material.mainTexture =
+                    cell.images[(int)cell.type.Value];
         }
 
-        private IEnumerator SwitchGridCell(BackgroundCellComponent backgroundCell)
+        private static IEnumerator SwitchGridCell(GameObject backgroundCell, Renderer renderer, Texture image)
         {
             const float animationTime = 30f;
             for (var i = 0; i < animationTime; i++)
@@ -81,13 +82,29 @@ namespace Systems.GridRendering
                 yield return null;
             }
 
-            backgroundCell.rendererCache.material.mainTexture = backgroundCell.images[(int)backgroundCell.type.Value];
+            renderer.material.mainTexture = image;
 
             for (var i = 0; i < animationTime; i++)
             {
                 backgroundCell.transform.RotateAround(backgroundCell.transform.position, Vector3.back, -3);
                 yield return null;
             }
+        }
+
+        public override void Register(ForegroundCellComponent component)
+        {
+            component.rendererCache = component.GetComponent<Renderer>();
+            component.type
+                .Subscribe(_ => AnimateForegroundCellChange(component))
+                .AddTo(component);
+        }
+
+        private static void AnimateForegroundCellChange(ForegroundCellComponent cell)
+        {
+            Observable.FromMicroCoroutine(() =>
+                    SwitchGridCell(cell.gameObject, cell.rendererCache, cell.images[(int)cell.type.Value]))
+                .Subscribe()
+                .AddTo(cell);
         }
     }
 }
