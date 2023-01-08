3dfd11441d7c61b78c8357a633fe796587da27d8
Michael Scherbakow <ryupold@gmail.com>
2023-01-08 22:50:59 +0100

    add harvest particles

---
+++ ourhillofstars/Assets/Systems/Drescher/DrescherParticleSystem.cs
@@ -0,0 +1,22 @@
+﻿using SystemBase.Core;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.Drescher
+{
+    [GameSystem]
+    public class DrescherParticleSystem : GameSystem<DrescherParticleComponent>
+    {
+        public override void Register(DrescherParticleComponent component)
+        {
+            var particleSystem = component.GetComponentInChildren<ParticleSystem>();
+            
+            MessageBroker.Default.Receive<HarvestedMsg>()
+                .Subscribe(msg =>
+                {
+                    particleSystem.Emit(40);
+                })
+                .AddTo(component);
+        }
+    }
+}
