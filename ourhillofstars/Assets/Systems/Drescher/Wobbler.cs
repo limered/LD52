b0cf8a68eab1df20d686ca80dacb90a6376aa4f3
Emil Bohleber <me@limered.de>
2023-01-09 15:36:56 +0100

    wobble the drescher

---
+++ ourhillofstars/Assets/Systems/Drescher/Wobbler.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+namespace Systems.Drescher
+{
+    public class Wobbler : MonoBehaviour
+    {
+        private void Update()
+        {
+            var scaleX = math.sin(Time.realtimeSinceStartup * 25) * 0.002f - 0.001f;
+            var scaleY = math.cos(Time.realtimeSinceStartup * 20) * 0.002f - 0.001f;
+            transform.localScale = new Vector3(scaleX + 0.1f, 1f, scaleY + 0.1f);
+        }
+    }
+}
